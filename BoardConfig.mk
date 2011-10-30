2872db83ccaf34ba4510034307020e5eab935163
Gerad Munsch <gmunsch@unforgivendevelopment.com>
2011-10-30 05:05:06 -0400

    Full, working recovery image is now able to be built right from CM build system
    To build recovery, do:
    $ source build/envsetup.sh
    $ lunch full_admire-eng
    $ make -j4 recoveryimage
    
    The resultant image can be flashed to your recovery partition -- a binary will be put up, and a full tutorial written in due time.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -4,19 +4,22 @@ USE_CAMERA_STUB := true
 -include vendor/samsung/admire/BoardConfigVendor.mk
 
 TARGET_NO_BOOTLOADER := true
-TARGET_BOARD_PLATFORM := unknown
-TARGET_CPU_ABI := armeabi
+TARGET_BOARD_PLATFORM := msm7k
+TARGET_ARCH_VARIANT := armv6-vfp
+TARGET_CPU_ABI := armeabi-v6l
+TARGET_CPU_ABI2 := armeabi
 TARGET_BOOTLOADER_BOARD_NAME := admire
 
 BOARD_KERNEL_CMDLINE := 
-BOARD_KERNEL_BASE := 0x13000000
+BOARD_KERNEL_BASE := 0x13008000
 BOARD_KERNEL_PAGESIZE := 4096
 
 # fix this up by examining /proc/mtd on a running device
-BOARD_BOOTIMAGE_PARTITION_SIZE := 0x00380000
-BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x00480000
-BOARD_SYSTEMIMAGE_PARTITION_SIZE := 0x08c60000
-BOARD_USERDATAIMAGE_PARTITION_SIZE := 0x105c0000
+BOARD_BOOTIMAGE_PARTITION_SIZE := 10485760
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 10485760
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 196083712
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 207618048
+
 BOARD_FLASH_BLOCK_SIZE := 131072
 
 TARGET_PREBUILT_KERNEL := device/samsung/admire/kernel
@@ -28,10 +31,16 @@ TARGET_PREBUILT_KERNEL := device/samsung/admire/kernel
 #TARGET_USERIMAGES_USE_EXT4 := true
 #TARGET_RECOVERY_INITRC := device/samsung/admire/recovery.rc
 BOARD_CUSTOM_RECOVERY_KEYMAPPING := ../../device/samsung/admire/recovery/recovery_ui.c
+BOARD_CUSTOM_GRAPHICS           := ../../../device/samsung/admire/recovery/graphics.c
 #BOARD_USES_BML_OVER_MTD := true
 #BOARD_CUSTOM_BOOTIMG_MK := device/samsung/aries-common/shbootimg.mk
 #TARGET_RECOVERY_PRE_COMMAND := "echo 1 > /cache/.startrecovery; sync;"
 BOARD_HAS_NO_SELECT_BUTTON := true
+TARGET_PREBUILT_RECOVERY_KERNEL := device/samsung/admire/recovery_kernel
+TARGET_RECOVERY_INITRC := device/samsung/admire/recovery.rc
+
+BOARD_BML_BOOT := "/dev/block/bml9"
+BOARD_BML_RECOVERY := "/dev/block/bml10"
 
 # Use this flag if the board has a ext4 partition larger than 2gb
 #BOARD_HAS_LARGE_FILESYSTEM := true
--- device_admire.mk
+++ device_admire.mk
@@ -17,8 +17,20 @@ endif
 PRODUCT_COPY_FILES += \
     $(LOCAL_KERNEL):kernel
 
+# Board-specific init
+PRODUCT_COPY_FILES += \
+    device/samsung/admire/ueventd.admire.rc:root/ueventd.rc \
+    device/samsung/admire/prebuilt/fsr.ko:root/lib/modules/fsr.ko \
+    device/samsung/admire/prebuilt/fsr_stl.ko:root/lib/modules/fsr_stl.ko \
+    device/samsung/admire/prebuilt/rfs_fat.ko:root/lib/modules/rfs_fat.ko \
+    device/samsung/admire/prebuilt/rfs_glue.ko:root/lib/modules/rfs_glue.ko \
+    device/samsung/admire/prebuilt/sec_param.ko:root/lib/modules/sec_param.ko \
+    device/samsung/admire/SCH-R720.rle:root/SCH-R720.rle \
+    device/samsung/admire/init.admire.rc:root/init.rc
+
 $(call inherit-product, build/target/product/full.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 PRODUCT_NAME := full_admire
 PRODUCT_DEVICE := admire
+PRODUCT_MODEL := SCH-R720
