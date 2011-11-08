c1651ffbc2f6a6a6c778c8393360fc3cfcee3381
Gerad Munsch <gmunsch@unforgivendevelopment.com>
2011-11-08 01:36:20 -0500

    Well, CM boots. But it's rather...broken. This is definitely a solid start. A *lot* of work will need to be done from here, to get things working correctly. If you want, you can go ahead and build this code from source, but I would definitely not anticipate having a usable phone.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,4 +1,4 @@
-# Copyright (C) 2007 The Android Open Source Project
+# Copyright (C) 2011 Gerad Munsch
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -26,16 +26,20 @@ TARGET_ARCH_VARIANT := armv6-vfp
 TARGET_CPU_ABI := armeabi-v6l
 TARGET_CPU_ABI2 := armeabi
 COPYBIT_MSM7K := true
-TARGET_BOARD_PLATFORM_GPU := qcom-adreno200
 TARGET_BOOTLOADER_BOARD_NAME := admire
+TARGET_OTA_ASSERT_DEVICE := admire,SCH-R720
+
+## GPU CONFIG ##
+TARGET_BOARD_PLATFORM_GPU := qcom-adreno200
 TARGET_LIBAGL_USE_GRALLOC_COPYBITS := true
+#-TARGET_USES_16BPPSURFACE_FOR_OPAQUE := true
+BOARD_EGL_CFG := device/samsung/admire/egl.cfg
 
 # ARMv6-compatible processor rev 5 (v6l)
 # XXX: I may switch to this config
 #TARGET_ARCH_VARIANT := armv6j
 #TARGET_CPU_ABI := armeabi-v6j
 #TARGET_CPU_ABI2 := armeabi
-
 
 ## BLUETOOTH ##
 BOARD_HAVE_BLUETOOTH := true
@@ -49,14 +53,16 @@ WIFI_DRIVER_MODULE_ARG      := ""
 WIFI_DRIVER_MODULE_NAME     := ar6000
 
 ## FM RADIO (temporarily disabled for testing) ##
-#BOARD_HAVE_FM_RADIO := true
-#BOARD_GLOBAL_CFLAGS += -DHAVE_FM_RADIO
-#BOARD_FM_DEVICE := bcm4325
+BOARD_HAVE_FM_RADIO := true
+BOARD_GLOBAL_CFLAGS += -DHAVE_FM_RADIO
+BOARD_FM_DEVICE := bcm4325
 
 ## MULTIMEDIA ##
-#TARGET_PROVIDES_LIBAUDIO := true
-BOARD_USES_GENERIC_AUDIO := false
-USE_CAMERA_STUB := true
+TARGET_PROVIDES_LIBAUDIO := true
+USE_CAMERA_STUB := false
+
+## MOBILE DATA ##
+BOARD_MOBILEDATA_INTERFACE_NAME := "pdp0"
 
 ## MISC ##
 WITH_JIT := true
@@ -75,41 +81,36 @@ BOARD_BOOTIMAGE_PARTITION_SIZE := 10485760
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 10485760
 BOARD_SYSTEMIMAGE_PARTITION_SIZE := 196083712
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 207618048
-BOARD_FLASH_BLOCK_SIZE := 4096
+BOARD_FLASH_BLOCK_SIZE := 131072
 
 ## PARTITION LAYOUT/INFO ##
 BOARD_DATA_DEVICE := /dev/block/bml13
 BOARD_DATA_FILESYSTEM := ext4
 BOARD_SYSTEM_DEVICE := /dev/block/bml12
-#BOARD_SYSTEM_FILESYSTEM := ext4
+BOARD_SYSTEM_FILESYSTEM := ext4
 BOARD_CACHE_DEVICE := /dev/block/bml14
-#BOARD_CACHE_FILESYSTEM := ext4
+BOARD_CACHE_FILESYSTEM := ext4
 BOARD_HAS_NO_MISC_PARTITION := true
 BOARD_BOOT_DEVICE := /devlblock/bml9
-BOARD_BML_BOOT := /dev/block/bml9
+BOARD_BML_BOOT := "/dev/block/bml9"
 BOARD_RECOVERY_DEVICE := /dev/block/bml10
-BOARD_BML_RECOVERY := /dev/block/bml10
-
+BOARD_BML_RECOVERY := "/dev/block/bml10"
 TARGET_USERIMAGES_USE_EXT4 := true
-
-BOARD_RECOVERY_HANDLES_MOUNT := true
 
 ## RECOVERY OPTIONS ##
 BOARD_CUSTOM_RECOVERY_KEYMAPPING := ../../device/samsung/admire/recovery/recovery_ui.c
 BOARD_CUSTOM_GRAPHICS           := ../../../device/samsung/admire/recovery/graphics.c
-#BOARD_USES_BML_OVER_MTD := true
-#BOARD_CUSTOM_BOOTIMG_MK := device/samsung/aries-common/shbootimg.mk
-#TARGET_RECOVERY_PRE_COMMAND := "echo 1 > /cache/.startrecovery; sync;"
 BOARD_HAS_NO_SELECT_BUTTON := true
 TARGET_PREBUILT_RECOVERY_KERNEL := device/samsung/admire/recovery_kernel
 TARGET_RECOVERY_INITRC := device/samsung/admire/recovery.rc
-#BOARD_USE_USB_MASS_STORAGE_SWITCH := true
-#BOARD_UMS_LUNFILE := "/sys/devices/platform/usb_mass_storage/lun0/file"
+BOARD_RECOVERY_HANDLES_MOUNT := true
+BOARD_USE_USB_MASS_STORAGE_SWITCH := true
+BOARD_UMS_LUNFILE := "/sys/devices/platform/usb_mass_storage/lun0/file"
 
 ## GPS ##
 BOARD_GPS_LIBRARIES := libloc_api
 BOARD_USES_QCOM_HARDWARE := true
 BOARD_USES_QCOM_LIBS := true
-BOARD_USES_QCOM_GPS := true		
+BOARD_USES_QCOM_GPS := true
 BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := admire
 BOARD_VENDOR_QCOM_GPS_LOC_API_AMSS_VERSION := 1240
--- device_admire.mk
+++ device_admire.mk
@@ -1,32 +1,72 @@
+# Copyright (C) 2011 Gerad Munsch
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# General config
 $(call inherit-product, build/target/product/full_base.mk)
 
-## GPS config ##
+# GPS config
 $(call inherit-product, device/common/gps/gps_us_supl.mk)
 
-## Device-specific overlay ##
+# Device-specific overlay
 DEVICE_PACKAGE_OVERLAYS := device/samsung/admire/overlay
 
-## HAL libs and other system binaries ##
+# Replace some inherited values with our own
+# Product information
+PRODUCT_NAME := admire
+PRODUCT_DEVICE := admire
+PRODUCT_MODEL := SCH-R720
+
+# Packages
+# XXX: I may move Gallery3d to vendor/samsung/admire/admire-vendor.mk
 PRODUCT_PACKAGES += \
+    Gallery3d \
+    SpareParts \
+    Development \
+    Term \
+    libOmxCore \
+    libOmxVidEnc \
+    FM \
+    make_ext4fs \
     brcm_patchram_plus \
+    sec_touchscreen.kcm \
     copybit.admire \
     gralloc.admire \
-    libOmxCore \
-    make_ext4fs \
-    dexpreopt \
-    screencap 
+    setup_fs \
+    dexpreopt
 
-## Use prebuilt kernel if possible ##
+# XXX: To be added to above list in time
+# screencap
+# FM
+# lights.admire
+
+# Use local (prebuilt) kernel if it exists
 ifeq ($(TARGET_PREBUILT_KERNEL),)
-	LOCAL_KERNEL := device/samsung/admire/kernel
+    LOCAL_KERNEL := device/samsung/admire/kernel
 else
-	LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
+    LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
 endif
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_KERNEL):kernel
 
-## Hardware properties 
+# Inherit stuff that is dependent on the proprietary drivers being present
+$(call inherit-product-if-exists, vendor/samsung/admire/admire-vendor.mk)
+
+# Enable dex-preopt
+DISABLE_DEXPREOPT := false
+
+# Hardware properties 
 PRODUCT_COPY_FILES += \
     frameworks/base/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
     frameworks/base/data/etc/android.hardware.camera.autofocus.xml:system/etc/permissions/android.hardware.camera.autofocus.xml \
@@ -37,7 +77,7 @@ PRODUCT_COPY_FILES += \
     frameworks/base/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
     frameworks/base/data/etc/android.hardware.touchscreen.multitouch.distinct.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.distinct.xml
 
-## Board-specific init ##
+# Board-specific init
 PRODUCT_COPY_FILES += \
     device/samsung/admire/ueventd.admire.rc:root/ueventd.rc \
     device/samsung/admire/prebuilt/fsr.ko:root/lib/modules/fsr.ko \
@@ -51,35 +91,57 @@ PRODUCT_COPY_FILES += \
     device/samsung/admire/init.qcom.rc:root/init.qcom.rc \
     device/samsung/admire/init.qcom.sh:root/init.qcom.sh
 
-## Keymaps ##
+# Keymaps
 PRODUCT_COPY_FILES += \
     device/samsung/admire/qwerty.kl:system/usr/keylayout/qwerty.kl \
     device/samsung/admire/sec_jack.kl:system/usr/keylayout/sec_jack.kl \
     device/samsung/admire/sec-key.kl:system/usr/keychars/sec-key.kl
 
-## WiFi stuff ##
+# WiFi stuff
 PRODUCT_COPY_FILES += \
     device/samsung/admire/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf \
     device/samsung/admire/hostapd.conf:system/etc/wifi/hostapd.conf \
     device/samsung/admire/dhcpcd.conf:system/etc/dhcpcd/dhcpcd.conf
 
-## Media stuff ##
+# Media stuff
 PRODUCT_COPY_FILES += \
     device/samsung/admire/AutoVolumeControl.txt:system/etc/AutoVolumeControl.txt \
     device/samsung/admire/AudioFilter.csv:system/etc/AudioFilter.csv \
     device/samsung/admire/vold.fstab:system/etc/vold.fstab \
     #device/samsung/admire/media_profiles.xml:system/etc/media_profiles.xml 
 
-## Property overrides ##
+# Property overrides
+PRODUCT_PROPERTY_OVERRIDES := \
+    keyguard.no_require_sim=true \
+    ro.com.android.dateformat=MM-dd-yyyy
+
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.sf.lcd_density=160 \
+    mobiledata.interfaces=pdp0,wlan0,gprs,ppp0 \
+    ro.telephony.ril_class=samsung \
+    wifi.interface=wlan0 \
+    wifi.supplicant_scan_interval=20 \
+    ro.com.android.dataroaming=false
+
+# screen density
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.sf.lcd_density=160
+
+# XXX: We might enable precise GC data
+#PRODUCT_TAGS += dalvik.gc.type-precise
+
+# This should not be needed, but on-screen keyboard uses the wrong density without it.
+PRODUCT_PROPERTY_OVERRIDES += \
     qemu.sf.lcd_density=160
 
-## Check for proprietary binaries ##
-$(call inherit-product-if-exists, vendor/samsung/admire/admire-vendor.mk)
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.com.google.locationfeatures=1 \
+    ro.setupwizard.enable_bypass=1 \
+    dalvik.vm.lockprof.threshold=500 \
+    dalvik.vm.dexopt-flags=m=y \
+    dalvik.vm.heapsize=32m \
+    dalvik.vm.dexopt-data-only=1 \
+    ro.opengles.version=131072 \
+    ro.compcache.default=0
 
-## Product over-rides and info ##
-PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
-PRODUCT_NAME := full_admire
-PRODUCT_DEVICE := admire
-PRODUCT_MODEL := SCH-R720
+# Inherit proprietary drivers, if they exist
+$(call inherit-product-if-exists, vendor/samsung/admire/admire-vendor-blobs.mk)
